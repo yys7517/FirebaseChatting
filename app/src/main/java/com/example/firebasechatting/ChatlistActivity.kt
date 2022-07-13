2f242151c7789ebe418ef19895249a88807eabb7
yys7517 <yys7517@naver.com>
2022-07-14 00:40:41 +0900

    로그인 성공 시, 채팅 Activity로 이동.

---
+++ app/src/main/java/com/example/firebasechatting/ChatlistActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firebasechatting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatlistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatlist)
+    }
+}
